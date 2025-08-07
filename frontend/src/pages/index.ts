4c4161fa39a8be16722a066e6afa2792199c2ed5
Melvyn Claes <claes.melvyn@gmail.com>
2025-08-07 21:29:29 +0200

    refactor(index): reorganize exports for better structure and clarity

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,8 +1,9 @@
 export * from './admin';
+export * from './organization';
+export * from './competition';
 export { default as Home } from './Home';
 export { default as NotFound } from './NotFound';
 export { SignInPage } from './SignIn';
 export { SignUpPage } from './SignUp';
 export { CompetitionsPage } from './Competitions';
 export { ResultsPage } from './Results';
-export { CompetitionDetailPage } from './CompetitionDetail';
