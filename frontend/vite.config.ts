220a2d9e8d37e6699c502ad1414998e60b30113b
Melvyn Claes <claes.melvyn@gmail.com>
2025-08-16 14:42:55 +0200

    feat: refactor Vite configuration to load environment variables and improve server setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,30 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    server: {
+      port: parseInt(env.VITE_PORT) || 5173,
     },
-  },
-  build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-          vendor: ['@tanstack/react-query', '@radix-ui/react-slot'],
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    build: {
+      chunkSizeWarningLimit: 1000,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            react: ['react', 'react-dom'],
+            vendor: ['@tanstack/react-query', '@radix-ui/react-slot'],
+          },
         },
       },
     },
-  },
+  };
 });
