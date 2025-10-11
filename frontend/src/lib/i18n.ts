06b13bc56ca81fed9430508f0c2a4033a310d5f1
Melvyn Claes <claes.melvyn@gmail.com>
2025-10-11 21:47:26 +0200

    feat: dynamically load namespaces from English translation files

--- frontend/src/lib/i18n.ts
+++ frontend/src/lib/i18n.ts
@@ -4,6 +4,12 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import resourcesToBackend from 'i18next-resources-to-backend';
 import { initReactI18next } from 'react-i18next';
 
+// Dynamically get namespaces from en translation files
+const enTranslationFiles = import.meta.glob('../translations/en/*.json', { eager: false });
+const namespaces = Object.keys(enTranslationFiles)
+  .map(path => path.match(/\/([^/]+)\.json$/)?.[1])
+  .filter((ns): ns is string => typeof ns === 'string');
+
 i18n
   .use(LanguageDetector)
   .use(
@@ -17,6 +23,7 @@ i18n
     debug: false,
     supportedLngs: SupportedLanguages,
     defaultNS: 'common',
+    ns: namespaces,
     interpolation: {
       escapeValue: false,
     },
