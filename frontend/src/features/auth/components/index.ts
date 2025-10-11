e1077fe16b04e10782bc8e78e8ca77ab5591babe
Melvyn Claes <claes.melvyn@gmail.com>
2025-10-11 16:53:24 +0200

    feat: add RequireAuth component and integrate it into the competition registration flow

--- frontend/src/features/auth/components/index.ts
+++ frontend/src/features/auth/components/index.ts
@@ -1,3 +1,4 @@
 export { AuthButton } from './auth-button';
+export { RequireAuth } from './require-auth';
 export { SignInForm } from './sign-in-form';
 export { SignUpForm } from './sign-up-form';
