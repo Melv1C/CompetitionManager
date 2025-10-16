b097a46484029efe7eea27407c06625c70bab043
riwan25 <riwan.claes@gmail.com>
2025-10-16 16:09:40 +0200

    wip 2

---
+++ desktop/src/electron/functions/getStatus.ts
@@ -0,0 +1,16 @@
+import { Competition } from "@repo/core/schemas";
+import { competitionExists } from "../sql/exist";
+
+export const getSatus = (competition: Competition) => {
+  competitionExists(competition.id);
+
+  //get all events already existing 
+  //TODO continue
+
+
+
+
+
+
+
+}
